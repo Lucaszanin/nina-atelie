b75e05da4158e68408195c1f73a4074ff050f0d2
unknown <lukinhas.so.lokura@hotmail.com>
2022-07-27 19:03:26 -0300

    Js de animações

---
+++ src/scripts/scrollreveal.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal({ reset: true })
+
+ScrollReveal().reveal('.cards', {
+    origin: 'bottom',
+    distance: '200px',
+    duration: 1500
+});
+
+ScrollReveal().reveal('.asset-section', {
+    origin: 'top',
+    distance: '150px',
+    duration: 1500
+});
+
+ScrollReveal().reveal('.img-contact', {
+    origin: 'top',
+    distance: '150px',
+    duration: 1500
+});
